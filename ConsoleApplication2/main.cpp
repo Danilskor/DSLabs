b48a6e74d049901d4fc89b0ed649e90e2c09a498
danil <danilskor2000@mail.ru>
2020-01-30 19:12:27 +0700

    Added first lab on master branch.

---
+++ ConsoleApplication2/main.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "ArrayHeader.h"
+
+
+int main()
+{
+	ArrayStucture structureArray;
+
+	structureArray.Capacity = 8;
+	structureArray.Length = 8;
+
+	structureArray.Array = ArrayCreation(&structureArray);
+
+	FillArray(&structureArray);
+
+	ArrayOutput(&structureArray);
+
+	Menu(&structureArray);
+	delete[] structureArray.Array;
+	return 0;
+}
