c48cbc2dc19deb7490aa5a91050bc517bbfb1b5c
danil <danilskor2000@mail.ru>
2020-01-30 19:22:34 +0700

    style(main): убраны лишние пустые строки

--- ConsoleApplication2/main.cpp
+++ ConsoleApplication2/main.cpp
@@ -5,14 +5,11 @@
 int main()
 {
 	ArrayStucture structureArray;
-
 	structureArray.Capacity = 8;
 	structureArray.Length = 8;
-
 	structureArray.Array = ArrayCreation(&structureArray);
 
 	FillArray(&structureArray);
-
 	ArrayOutput(&structureArray);
 
 	Menu(&structureArray);
